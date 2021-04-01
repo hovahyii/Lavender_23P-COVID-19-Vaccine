99f93cf2967a3dfc54350982a1e585044cfb59ca
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2021-04-01 17:53:36 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ module.exports = {
     publicRuntimeConfig:{
         APP_NAME: 'VACCINE',
         API_DEVELOPMENT: 'http://localhost:8000/api',
-        API_PRODUCTION: 'http://www.lavender23p-covid-19-vaccine.gq/api',
+        API_PRODUCTION: 'https://lavender23p-covid-19-vaccine.herokuapp.com/api',
         PRODUCTION: true,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-        DOMAIN_PRODUCTION: 'http://www.lavender23p-covid-19-vaccine.gq/',
+        DOMAIN_PRODUCTION: 'https://lavender23p-covid-19-vaccine.herokuapp.com',
         DISQUS_SHORTNAME: 'covid-19-vaccine',
         GOOGLE_CLIENT_ID: '320727890298-pa7d5h1qq0e3vusg588fa0cgondh5nsq.apps.googleusercontent.com'
     }
